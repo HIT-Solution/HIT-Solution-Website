53539d7f83d810b15a850d879ea5f95e65b993ed
Sakhawat Hosen <sakhawathosen320@gmail.com>
2024-06-06 00:28:51 +0600

    text decoration chage daynami

--- src/Pages/Home/Portfolio.js
+++ src/Pages/Home/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import service1 from "../assets/img/portfolio/app-1.jpg";
 import service2 from "../assets/img/portfolio/product-1.jpg";
 import service3 from "../assets/img/portfolio/branding-1.jpg";
@@ -13,11 +13,33 @@ import service11 from "../assets/img/portfolio/branding-3.jpg";
 import service12 from "../assets/img/portfolio/books-3.jpg";
 
 const Portfolio = () => {
+  const [activeFilter, setActiveFilter] = useState("*");
+
+  const portfolioItems = [
+    { id: 1, category: "filter-app", img: service1, title: "App 1" },
+    { id: 2, category: "filter-product", img: service2, title: "Product 1" },
+    { id: 3, category: "filter-branding", img: service3, title: "Branding 1" },
+    { id: 4, category: "filter-books", img: service4, title: "Books 1" },
+    { id: 5, category: "filter-app", img: service5, title: "App 2" },
+    { id: 6, category: "filter-product", img: service6, title: "Product 2" },
+    { id: 7, category: "filter-branding", img: service7, title: "Branding 2" },
+    { id: 8, category: "filter-books", img: service8, title: "Books 2" },
+    { id: 9, category: "filter-app", img: service9, title: "App 3" },
+    { id: 10, category: "filter-product", img: service10, title: "Product 3" },
+    { id: 11, category: "filter-branding", img: service11, title: "Branding 3" },
+    { id: 12, category: "filter-books", img: service12, title: "Books 3" },
+  ];
+
+  const filteredItems =
+    activeFilter === "*"
+      ? portfolioItems
+      : portfolioItems.filter(item => item.category === activeFilter);
+
   return (
     <div>
-      <section id="portfolio" class="portfolio sections-bg">
-        <div class="container" data-aos="fade-up">
-          <div class="section-header">
+      <section id="portfolio" className="portfolio sections-bg">
+        <div className="container" data-aos="fade-up">
+          <div className="section-header">
             <h2>Portfolio</h2>
             <p>
               Quam sed id excepturi ccusantium dolorem ut quis dolores nisi llum
@@ -26,7 +48,7 @@ const Portfolio = () => {
           </div>
 
           <div
-            class="portfolio-isotope"
+            className="portfolio-isotope"
             data-portfolio-filter="*"
             data-portfolio-layout="masonry"
             data-portfolio-sort="original-order"
@@ -34,257 +56,65 @@ const Portfolio = () => {
             data-aos-delay="100"
           >
             <div>
-              <ul class="portfolio-flters">
-                <li data-filter="*" class="filter-active">
+              <ul className="portfolio-flters">
+                <li
+                  className={activeFilter === "*" ? "filter-active" : ""}
+                  onClick={() => setActiveFilter("*")}
+                >
                   All
                 </li>
-                <li data-filter=".filter-app">App</li>
-                <li data-filter=".filter-product">Product</li>
-                <li data-filter=".filter-branding">Branding</li>
-                <li data-filter=".filter-books">Books</li>
+                <li
+                  className={activeFilter === "filter-app" ? "filter-active" : ""}
+                  onClick={() => setActiveFilter("filter-app")}
+                >
+                  App
+                </li>
+                <li
+                  className={activeFilter === "filter-product" ? "filter-active" : ""}
+                  onClick={() => setActiveFilter("filter-product")}
+                >
+                  Product
+                </li>
+                <li
+                  className={activeFilter === "filter-branding" ? "filter-active" : ""}
+                  onClick={() => setActiveFilter("filter-branding")}
+                >
+                  Branding
+                </li>
+                <li
+                  className={activeFilter === "filter-books" ? "filter-active" : ""}
+                  onClick={() => setActiveFilter("filter-books")}
+                >
+                  Books
+                </li>
               </ul>
             </div>
 
-            <div class="row gy-4 portfolio-container">
-              <div class="col-xl-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/app-1.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service1} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        App 1
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+            <div className="row gy-4 portfolio-container">
+              {filteredItems.map(item => (
+                <div
+                  key={item.id}
+                  className={`col-xl-4 col-md-6 portfolio-item ${item.category}`}
+                >
+                  <div className="portfolio-wrap">
+                    <a
+                      href={`assets/img/portfolio/${item.img}`}
+                      data-gallery="portfolio-gallery-app"
+                      className="glightbox text-decoration-none"
+                    >
+                      <img src={item.img} className="img-fluid" alt="" />
+                    </a>
+                    <div className="portfolio-info">
+                      <h4>
+                        <a href="portfolio-details.html" title="More Details" className="text-decoration-none">
+                          {item.title}
+                        </a>
+                      </h4>
+                      <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-product">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/product-1.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service2} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Product 1
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/branding-1.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service3} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Branding 1
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-books">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/books-1.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service4} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Books 1
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/app-2.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service5} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        App 2
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-product">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/product-2.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service6} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Product 2
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/branding-2.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service7} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Branding 2
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-books">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/books-2.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service8} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Books 2
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/app-3.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service9} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        App 3
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-product">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/product-3.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service10} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Product 3
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/branding-3.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service11} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Branding 3
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 portfolio-item filter-books">
-                <div class="portfolio-wrap">
-                  <a
-                    href="assets/img/portfolio/books-3.jpg"
-                    data-gallery="portfolio-gallery-app"
-                    class="glightbox"
-                  >
-                    <img src={service12} class="img-fluid" alt="" />
-                  </a>
-                  <div class="portfolio-info">
-                    <h4>
-                      <a href="portfolio-details.html" title="More Details">
-                        Books 3
-                      </a>
-                    </h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur</p>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
--- src/Pages/Shared/Fotter.js
+++ src/Pages/Shared/Fotter.js
@@ -1,77 +1,70 @@
 import React from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 
-const Fotter = () => {
+const Footer = () => {
   return (
-   
-    <footer id="footer" class="footer">
+    <footer id="footer" className="footer">
+      <div className="container">
+        <div className="row row-cols-4 gy-4">
+          <div className="col-lg-5 col-md-12 footer-info text-center text-md-start">
+            <a href="index.html" className="logo d-flex align-items-center text-decoration-none">
+              <span>Hasan's IT Solution</span>
+            </a>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+            <div className="social-links d-flex mt-4">
+              <a href="#" className="twitter text-decoration-none"><i className="bi bi-twitter"></i></a>
+              <a href="#" className="facebook text-decoration-none"><i className="bi bi-facebook"></i></a>
+              <a href="#" className="instagram text-decoration-none"><i className="bi bi-instagram"></i></a>
+              <a href="#" className="linkedin text-decoration-none"><i className="bi bi-linkedin"></i></a>
+            </div>
+          </div>
 
-    <div class="container">
-      <div class="row gy-4">
-        <div class="col-lg-5 col-md-12 footer-info">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span>Hasan's IT Solution</span>
-          </a>
-          <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-          <div class="social-links d-flex mt-4">
-            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          <div className="col-lg-2 col-6 footer-links text-center text-md-start">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#" className="text-decoration-none">Home</a></li>
+              <li><a href="#" className="text-decoration-none">About us</a></li>
+              <li><a href="#" className="text-decoration-none">Services</a></li>
+              <li><a href="#" className="text-decoration-none">Terms of service</a></li>
+              <li><a href="#" className="text-decoration-none">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div className="col-lg-2 col-6 footer-links text-center text-md-start">
+            <h4 className='text-left'>Our Services</h4>
+            <ul>
+              <li><a href="#" className="text-decoration-none">Web Design</a></li>
+              <li><a href="#" className="text-decoration-none">Web Development</a></li>
+              <li><a href="#" className="text-decoration-none">Product Management</a></li>
+              <li><a href="#" className="text-decoration-none">Marketing</a></li>
+              <li><a href="#" className="text-decoration-none">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div className="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br/>
+              New York, NY 535022<br/>
+              United States <br/><br/>
+              <strong>Phone:</strong> +8801950090248<br/>
+              <strong>Email:</strong> hasansit48@gmail.com<br/>
+            </p>
           </div>
         </div>
-
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-          <h4>Contact Us</h4>
-          <p>
-            A108 Adam Street <br/>
-            New York, NY 535022<br/>
-            United States <br/><br/>
-            <strong>Phone:</strong> +8801950090248<br/>
-            <strong>Email:</strong> hasansit48@gmail.com<br/>
-          </p>
-
-        </div>
-
       </div>
-    </div>
 
-    <div class="container mt-4">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Hasan's IT Solution</span></strong>. All Rights Reserved
+      <div className="container mt-4">
+        <div className="copyright">
+          &copy; Copyright 2024 <strong><span>Hasan's IT Solution</span></strong>. All Rights Reserved
+        </div>
+        <div className="credits">
+          Designed by <a href="https://bootstrapmade.com/" className="text-decoration-none">sakhwathosen</a>
+        </div>
       </div>
-      <div class="credits">
-       
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-
-  </footer>
-      
- 
+    </footer>
   );
 };
 
-export default Fotter;
+export default Footer;
